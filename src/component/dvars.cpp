fadd4c144629707ce28160a7548f78f5bc067ae9
Diavolo <edoardo.sanguineti222@gmail.com>
2021-06-24 15:51:52 +0200

    Tabs per usual

--- src/component/dvars.cpp
+++ src/component/dvars.cpp
@@ -11,27 +11,27 @@ namespace dvars
 	game::dvar_t* sv_EnableGameChat;
 
 	game::dvar_t* Dvar_RegisterBool(const std::string &name, const bool value, const unsigned short flags,
-						   const std::string &description)
+						const std::string &description)
 	{
 		return game::Dvar_RegisterBool(name.data(), value, flags, description.data());
 	}
 
 	game::dvar_t* Dvar_RegisterFloat(const std::string &name, const float value, const float min, const float max,
-							const unsigned short flags,
-							const std::string &description)
+						const unsigned short flags,
+						const std::string &description)
 	{
 		return game::Dvar_RegisterFloat(name.data(), value, min, max, flags, description.data());
 	}
 
 	game::dvar_t* Dvar_RegisterInt(const std::string &name, const int value, const int min, const int max,
-						  const unsigned short flags,
-						  const std::string &description)
+						const unsigned short flags,
+						const std::string &description)
 	{
 		game::Dvar_RegisterInt(name.data(), value, min, max, flags, description.data());
 	}
 
 	game::dvar_t* Dvar_RegisterString(const std::string &name, const std::string &value, const unsigned short flags,
-							 const std::string &description)
+						const std::string &description)
 	{
 		game::Dvar_RegisterString(name.data(), value.data(), flags, description.data());
 	}
