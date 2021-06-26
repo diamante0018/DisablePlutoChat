d4e37b4b01cb692557d209d39d8f14a7acea6fb5
Diavolo <edoardo.sanguineti222@gmail.com>
2021-06-26 11:14:02 +0200

    Save dsr

--- src/component/dsr.cpp
+++ src/component/dsr.cpp
@@ -12,13 +12,22 @@ namespace dsr
     namespace
     {
         utils::hook::detour open_dsr_hook;
+        utils::hook::detour save_dsr_hook;
 
-        bool open_DSR_stub(char *dsr_name)
+        bool open_DSR_stub(const char *dsr_name)
         {
             printf("Loading %s.dsr\n", dsr_name);
             dvars::Dvar_RegisterString("sv_dsr", dsr_name, game::DVAR_FLAG_NONE, "Last DSR opened by the game");
 
             return open_dsr_hook.invoke<bool>(dsr_name);
+        }
+
+        bool save_DSR_stub(void* a1, const char* dsr_name)
+        {
+            printf("Saving %s.dsr\n", dsr_name);
+            dvars::Dvar_RegisterString("sv_saved_dsr", dsr_name, game::DVAR_FLAG_NONE, "Last DSR saved by the game");
+
+            return save_dsr_hook.invoke<bool>(a1, dsr_name);
         }
     }
 
@@ -28,6 +37,7 @@ namespace dsr
         void post_unpack() override
         {
             open_dsr_hook.create(0x06BB470, &open_DSR_stub);
+            save_dsr_hook.create(0x06BB4F0, &save_DSR_stub);
         }
     };
 }
