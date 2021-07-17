8326494874b60ccea8270609325d3737661f6ba5
Diavolo <edoardo.sanguineti222@gmail.com>
2021-07-17 10:19:32 +0200

    Dvars implemented

--- src/component/collision.cpp
+++ src/component/collision.cpp
@@ -7,10 +7,26 @@
 
 namespace collision
 {
+	game::dvar_t* g_playerCollision;
 	__declspec(naked) void player_collision_stub()
 	{
 		__asm
 		{
+			push eax;
+			mov eax, g_playerCollision;
+			cmp byte ptr[eax + 12], 0;
+			pop eax;
+
+			je dontcollide;
+
+			mov eax, dword ptr[esp + 0xa0];
+			jmp collide;
+
+		collide:
+			push 0x05413B6;
+			retn;
+
+		dontcollide:
 			mov eax, dword ptr[esp + 0xa0];
 			mov ecx, dword ptr[esp + 9ch];
 			push eax;
@@ -23,10 +39,26 @@ namespace collision
 		}
 	}
 
+	game::dvar_t* g_playerEjection;
 	__declspec(naked) void player_ejection_stub()
 	{
 		__asm
 		{
+			push eax;
+			mov eax, g_playerEjection;
+			cmp byte ptr[eax + 12], 0;
+			pop eax;
+
+			je donteject;
+
+			cmp dword ptr[ebx + 19ch], edi;
+			jle eject;
+
+		eject:
+			push 0x04F9EFA;
+			retn;
+
+		donteject:
 			push 0x04F9F03;
 			retn;
 		}
@@ -37,6 +69,9 @@ namespace collision
 	public:
 		void post_unpack() override
 		{
+			g_playerCollision = game::Dvar_RegisterBool("g_playerCollision", true, game::DVAR_FLAG_NONE, "Flag whether player collision is on or off");
+			g_playerEjection = game::Dvar_RegisterBool("g_playerEjection", true, game::DVAR_FLAG_NONE, "Flag whether player ejection is on or off");
+
 			utils::hook::jump(0x05413AF, player_collision_stub);
 			utils::hook::jump(0x04F9EF2, player_ejection_stub);
 		}
